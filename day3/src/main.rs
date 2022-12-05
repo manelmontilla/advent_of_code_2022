664f7b0984274b9639160a7ac35e7d0145d44b07
Manel Montilla <manel.montilla@gmail.com>
2022-12-06 00:30:26 +0100

    Day 3 first half of the puzzle

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,30 +1,66 @@
-use std::env;
-use std::fs;
+use std::{env, fs};
+
 fn main() {
     solve1();
 }
 
+static A: u8 = b'A';
+static Z: u8 = b'Z';
+static a: u8 = b'a';
+static z: u8 = b'z';
+
 struct Item(u8);
 impl Item {
     fn priority(&self) -> u8 {
+        let r = 1..57;
+        if self.0 <= Z && self.0 >= A {
+            return self.0 - A + 27;
+        };
+        self.0 - a + 1
+    }
 
+    fn from_priority(priority: u8) -> Self {
+        if priority >= 1 && priority <= 26 {
+            return Item(a + (priority - 1));
+        };
+        Item(A + (priority - 1))
     }
 }
 
 impl From<u8> for Item {
     fn from(val: u8) -> Self {
-        if  val <= b'Z' && val >= b'A'  {
-                return Item(val)
+        if val <= b'Z' && val >= b'A' {
+            return Item(val);
         };
-        if  val <= b'z' && val >= b'a'  {
-            return Item(val)
+        if val <= b'z' && val >= b'a' {
+            return Item(val);
         };
         panic!("invalid item type {}", val.to_string())
     }
 }
 
-struct RuckSack {
-    
+struct Rucksack {
+    repeated: Item,
+}
+
+impl From<&str> for Rucksack {
+    fn from(s: &str) -> Self {
+        let (first_half, second_half) = s.as_bytes().split_at(s.len() / 2);
+        let mut found: [bool; 52] = [false; 52];
+        let half_len = first_half.len();
+        for i in 0..half_len {
+            let item_str = first_half[i];
+            let item = Item::from(item_str);
+            found[item.priority() as usize - 1] = true;
+        }
+        for i in 0..half_len {
+            let item = Item::from(second_half[i]);
+            if found[item.priority() as usize - 1] {
+                return Rucksack { repeated: item };
+            }
+        }
+        panic!("no repeated item found in rucksack {}", s);
+    }
 }
 
 fn solve1() {
@@ -32,10 +68,39 @@ fn solve1() {
     let input = &input[1];
     let content = fs::read_to_string(input).unwrap();
     let lines = content.split('\n').collect::<Vec<&str>>();
-    let au = b'A';
-    let al = b'a';
-    let zu = b'Z';
-    let zl = b'z';
-    println!("a priority: {}, z priority: {}",  al - al+ 1,  zl-al + 1);
-    println!("A priority: {}, Z priority: {}",  26 + au - au + 1, zu - au + 1 + 26, );
+    let mut sum_repeated_priorities: usize = 0;
+    for line in lines {
+        if line.len() == 0 {
+            continue;
+        }
+        let rucksack = Rucksack::from(line);
+        sum_repeated_priorities = sum_repeated_priorities + rucksack.repeated.priority() as usize;
+    }
+    println!(
+        "sum of the repeated priorities in each rucksack {}",
+        sum_repeated_priorities
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ruskstack() {
+        let r = Rucksack::from("vJrwpWtwJgWrhcsFMMfFFhFp");
+        assert_eq!(r.repeated.0, b'p');
+
+        let r = Rucksack::from("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL");
+        assert_eq!(r.repeated.0, b'L');
+    }
+
+    #[test]
+    fn test_priority() {
+        let item = Item::from(b'p');
+        assert_eq!(item.priority(), 16);
+
+        let item = Item::from(b'L');
+        assert_eq!(item.priority(), 38);
+    }
 }
